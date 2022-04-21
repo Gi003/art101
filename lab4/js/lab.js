fafc3558584a312e400a6c7bbb3e35d85ecb91f9
gi003 <diraheta@ucsc.edu>
2022-04-21 13:10:22 -0700

    Completed lab 5

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+// Creating a digital game of tag:
+// 1. Create two objects, both will be players
+  // 2. Accept user input in order to create controllability in mobility for both separate players
+    // 3.. Assign a value to create the “it”
+  // 4. Give player 1 a true value for being it
+// 5. Give player 2 a false value for being it
+// 6. Set up a condition so that if the proximity of both objects is zero then both of the objects’ individual values will change.
---
+++ lab5/js/lab.js
@@ -0,0 +1,31 @@
+// Author:  Gio Iraheta
+// Created: 04.15.2022
+
+//Setting variable for the make of the Car
+var make = "Toyota";
+
+//Setting variable for the model
+var model = "Tacoma";
+
+//Setting variable for the color
+var color = "Grey";
+
+//Setting variable for the year
+var year = 2003;
+
+//Setting variable for boolean of ownership
+var ownIt = true;
+
+
+
+//calculating the age of the car
+var age = 2022 - year;
+
+
+//output
+
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writeln("Age:" + age + "<br>");
